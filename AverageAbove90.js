6e3296958f5d56cdb88587042f2edd4e3150219f
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2023-09-19 16:49:33 +0530

    new program

---
+++ AverageAbove90.js
@@ -0,0 +1,18 @@
+const students = [
+  { name: "Alice", scores: [90, 85, 92] },
+  { name: "Bob", scores: [75, 80, 85] },
+  { name: "Charlie", scores: [90, 95, 85] },
+  { name: "Jack", scores: [100, 100, 100] },
+];
+
+// Use map to calculate the average test score for each student
+const studentAverages = students.map((student) => {
+  const sum = student.scores.reduce((acc, score) => acc + score);
+  return { name: student.name, average: sum / student.scores.length };
+});
+
+// Use filter to only select students with an average above 90
+const highPerformers = studentAverages.filter(
+  (student) => student.average > 90
+);
+console.log(highPerformers);
